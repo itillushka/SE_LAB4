d3bd4399297283a07fb53aedbec9687fa0950d28
illia@ITIllushka <276487@student.pwr.edu.pl>
2024-11-06 10:41:41 +0100

    Part 5 migrate completed

--- lab4_exercise/helloWorldDjango/models.py
+++ lab4_exercise/helloWorldDjango/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Customer(models.Model):
-    id = models.AutoField(primary_key=True)         
-    name = models.CharField(max_length=255)          
+    id = models.AutoField(primary_key=True)          
+    name = models.CharField(max_length=100)          
     address = models.TextField()                     
 
     def __str__(self):
@@ -10,21 +10,28 @@ class Customer(models.Model):
 
 
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)          
-    name = models.CharField(max_length=255)          
-    price = models.FloatField()                      
-    available = models.BooleanField()               
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)        
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    available = models.BooleanField()                
 
     def __str__(self):
         return self.name
 
 
 class Order(models.Model):
-    id = models.AutoField(primary_key=True)          
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE) 
-    products = models.ManyToManyField(Product)      
-    date = models.DateTimeField()                    
-    status = models.CharField(max_length=50)        
+    STATUS_CHOICES = [
+        ('New', 'New'),
+        ('In Process', 'In Process'),
+        ('Sent', 'Sent'),
+        ('Completed', 'Completed')
+    ]
+
+    id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES) 
 
     def __str__(self):
         return f"Order {self.id} by {self.customer.name}"
