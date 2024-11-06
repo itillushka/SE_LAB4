f319360c77b6d5474d02f5561be4b77b50eccf30
illia@ITIllushka <276487@student.pwr.edu.pl>
2024-11-06 10:45:15 +0100

    Part 5 completed

--- lab4_exercise/helloWorldDjango/models.py
+++ lab4_exercise/helloWorldDjango/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.db.models import Sum
 
 class Customer(models.Model):
-    id = models.AutoField(primary_key=True)          
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)          
     address = models.TextField()                     
 
@@ -11,12 +13,20 @@ class Customer(models.Model):
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)        
-    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    name = models.CharField(max_length=255)          
+    price = models.DecimalField(max_digits=10, decimal_places=2)  
     available = models.BooleanField()                
 
     def __str__(self):
         return self.name
+
+    
+    def save(self, *args, **kwargs):
+        if self.price <= 0:
+            raise ValidationError("Price must be a positive value.")
+        else:
+            self.clean()  
+            super().save(*args, **kwargs)
 
 
 class Order(models.Model):
@@ -31,7 +41,14 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     date = models.DateTimeField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES) 
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
 
     def __str__(self):
         return f"Order {self.id} by {self.customer.name}"
+
+    def calculate_total_price(self):
+        total_price = self.products.aggregate(total=Sum('price'))['total'] or 0
+        return total_price
+
+    def can_be_fulfilled(self):
+        return all(product.available for product in self.products.all())
