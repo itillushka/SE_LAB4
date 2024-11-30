22db4c1d67b5f9fe42072b7d6461be9d3fd9aa25
illia@ITIllushka <276487@student.pwr.edu.pl>
2024-11-30 14:12:55 +0100

    Part 3

--- django_project/django_app/urls.py
+++ django_project/django_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CustomerViewSet, OrderViewSet
+from .views import ProductListView, ProductDetailView, ProductCreateView
+
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
@@ -9,4 +11,10 @@ router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('user/products/', ProductListView.as_view(),
+    name='product_list'),
+    path('user/products/<int:pk>/', ProductDetailView.as_view(),
+    name='product_detail'),
+    path('user/products/new/', ProductCreateView.as_view(),
+    name='product_create'),
 ]
