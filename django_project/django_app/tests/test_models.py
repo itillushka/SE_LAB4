d2e51c74db5ffdba5d0f2bf16ed594d4cba491cc
illia@ITIllushka <276487@student.pwr.edu.pl>
2024-12-19 18:38:58 +0100

    Part 2

--- django_project/django_app/tests/test_models.py
+++ django_project/django_app/tests/test_models.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
-from django_app.models import Product, Customer, Order
 from django.core.exceptions import ValidationError
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from django_app.models import Product, Customer, Order
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import AccessToken
 
 class ProductModelTest(TestCase):
 
@@ -118,3 +123,161 @@ class OrderModelTest(TestCase):
         order.products.remove(self.product2)
         can_fulfill = all(product.available for product in order.products.all())
         self.assertTrue(can_fulfill)
+
+class ProductApiTest(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.regular_user = User.objects.create_user(username='testuser', password='testpassword')
+        self.admin = User.objects.create_superuser(username='testadmin', password='testpassword')
+        self.product = Product.objects.create(name='Temporary Product', price=1.99, available=True)
+        self.product_list_url = reverse('product-list')
+        self.product_detail_url = reverse('product-detail', args=[self.product.id])
+
+    def test_get_all_products_as_regular_user(self):
+        self.token = str(AccessToken.for_user(self.regular_user))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.get(self.product_list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], 'Temporary Product')
+        self.assertEqual(response.data[0]['price'], '1.99')
+        self.assertTrue(response.data[0]['available'])
+
+    def test_get_all_products_as_admin(self):
+        self.token = str(AccessToken.for_user(self.admin))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.get(self.product_list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], 'Temporary Product')
+        self.assertEqual(response.data[0]['price'], '1.99')
+        self.assertTrue(response.data[0]['available'])
+
+    def test_get_single_product_as_regular_user(self):
+        self.token = str(AccessToken.for_user(self.regular_user))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.get(self.product_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Temporary Product')
+        self.assertEqual(response.data['price'], '1.99')
+        self.assertTrue(response.data['available'])
+
+    def test_get_single_product_as_admin(self):
+        self.token = str(AccessToken.for_user(self.admin))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.get(self.product_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Temporary Product')
+        self.assertEqual(response.data['price'], '1.99')
+        self.assertTrue(response.data['available'])
+
+    def test_create_product_with_valid_data_as_regular_user(self):
+        self.token = str(AccessToken.for_user(self.regular_user))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        data = {"name": "Temporary Product 2", "price": 4.99, "available": True}
+        response = self.client.post(self.product_list_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_product_with_valid_data_as_admin(self):
+        self.token = str(AccessToken.for_user(self.admin))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        data = {"name": "Temporary Product 2", "price": 4.99, "available": True}
+        response = self.client.post(self.product_list_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['name'], 'Temporary Product 2')
+        self.assertEqual(response.data['price'], '4.99')
+        self.assertTrue(response.data['available'])
+
+    def test_modify_product_with_valid_data_as_regular_user(self):
+        self.token = str(AccessToken.for_user(self.regular_user))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        data = {"name": "Modified Product"}
+        response = self.client.patch(self.product_detail_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_modify_product_with_valid_data_as_admin(self):
+        self.token = str(AccessToken.for_user(self.admin))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        data = {"name": "Modified Product"}
+        response = self.client.patch(self.product_detail_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Modified Product')
+        self.assertEqual(response.data['price'], '1.99')
+        self.assertTrue(response.data['available'])
+
+    def test_delete_product_as_regular_user(self):
+        self.token = str(AccessToken.for_user(self.regular_user))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.delete(self.product_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_product_as_admin(self):
+        self.token = str(AccessToken.for_user(self.admin))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.delete(self.product_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Product.objects.count(), 0)
+
+class ProductApiNegativeTest(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.regular_user = User.objects.create_user(username='testuser', password='testpassword')
+        self.admin = User.objects.create_superuser(username='testadmin', password='testpassword')
+        self.product = Product.objects.create(name='Temporary Product', price=1.99, available=True)
+        self.product_list_url = reverse('product-list')
+        self.product_detail_url = reverse('product-detail', args=[self.product.id])
+        self.invalid_product_detail_url = reverse('product-detail', args=[999])
+
+    def test_create_product_with_invalid_data_as_admin(self):
+        self.token = str(AccessToken.for_user(self.admin))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        data = {"name": "", "price": -1, "available": True}
+        response = self.client.post(self.product_list_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_product_with_invalid_data_as_regular_user(self):
+        self.token = str(AccessToken.for_user(self.regular_user))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        data = {"name": "", "price": -1, "available": True}
+        response = self.client.post(self.product_list_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_nonexistent_product_as_regular_user(self):
+        self.token = str(AccessToken.for_user(self.regular_user))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.get(self.invalid_product_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_nonexistent_product_as_admin(self):
+        self.token = str(AccessToken.for_user(self.admin))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.get(self.invalid_product_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_modify_product_with_invalid_data_as_admin(self):
+        self.token = str(AccessToken.for_user(self.admin))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        data = {"name": "", "price": -1}
+        response = self.client.patch(self.product_detail_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_modify_product_with_invalid_data_as_regular_user(self):
+        self.token = str(AccessToken.for_user(self.regular_user))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        data = {"name": "", "price": -1}
+        response = self.client.patch(self.product_detail_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_nonexistent_product_as_regular_user(self):
+        self.token = str(AccessToken.for_user(self.regular_user))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.delete(self.invalid_product_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_nonexistent_product_as_admin(self):
+        self.token = str(AccessToken.for_user(self.admin))
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.delete(self.invalid_product_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
